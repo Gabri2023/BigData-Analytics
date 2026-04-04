6958cb8f1b6b6f7ba3f852b403ea31423da1a104
Gabri2023 <s1126919@studenti.univpm.it>
2026-04-04 18:54:20 +0200

    aggiunta B2_extreme_max

--- main.py
+++ main.py
@@ -110,8 +110,10 @@ def main():
         # --- B SCENARIOS ---
         case "B1_exact":
             target_anomalies = filter_by_ged(features_dict, exact_ged=config['ged_thresholds']['exact'])
-        case "B1_extreme":
+        case "B1_extreme_min":
             target_anomalies = filter_by_ged(features_dict, min_ged=config['ged_thresholds']['min_extreme'])
+        case "B2_extreme_max":
+            target_anomalies = filter_by_ged(features_dict, max_ged=config['ged_thresholds']['max_extreme'])
         case "B2_bottleneck":
             target_anomalies = filter_by_bottleneck(features_dict, anom_graphs, auto_bottlenecks)
         case "B3_early":
